ec8178f11bca76739ddad9d9875c3df8bc913395
shatfield4 <seanhatfield5@gmail.com>
2023-02-27 14:09:16 -0800

    fixed footer design for mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Image from 'next/image';
 import 'tailwindcss/tailwind.css';
 import ComingSoon from './componenents/ComingSoon';
-import Footer from './componenents/Footer';
+import Footer from './componenents/FooterBar';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
