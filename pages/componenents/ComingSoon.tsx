f0afb7b5e593e58d5af01a8b37ed184ae4661fcb
shatfield4 <seanhatfield5@gmail.com>
2023-02-28 09:42:12 -0800

    changed button ui

--- pages/componenents/ComingSoon.tsx
+++ pages/componenents/ComingSoon.tsx
@@ -35,30 +35,17 @@ const ComingSoon = () => {
         No more manual responses, no more missed emails.
       </div>
       <Button buttonText={'Coming Soon'} showButton={showButton} onClick={handleSignUpEarlyAccess}/>
-      {/* <div className="flex justify-center my-8 w-full md:w-2/3 px-5 mt-16">
-        <div className={`rounded-xl p-2 shadow-purple-glow inset-1 transition-opacity duration-1000 ${showImage ? 'opacity-100' : 'opacity-0'}`}>
-          <Image
-            src="/dashboard.png"
-            alt="Dashboard"
-            width={1200}
-            height={800}
-            className="rounded-lg"
-          />
-        </div>
-      </div> */}
-
-      {/* <div className=`flex justify-center my-8 w-full md:w-2/3 px-5 mt-16 transition-opacity duration-1000 ${showImage ? 'opacity-100' : 'opacity-0'}`> */}
       <div className={`flex justify-center px-8 my-16 transition-opacity duration-1000 ${showImage ? 'opacity-100' : 'opacity-0'}`}>
         <div className="gap-8 items-start flex justify-center w-full md:w-2/3">
           <div className="relative group">
-            <div className="absolute -inset-0 bg-purple-500 rounded-xl blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-            <button className="relative bg-black rounded-xl leading-none flex items-center divide-x divide-gray-600 text-white">
+            <div className="absolute -inset-0 bg-gradient-to-r from-blue-600 to-purple-600 rounded-2xl blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+            <button className="relative bg-black rounded-2xl leading-none flex items-center divide-x divide-gray-600 text-white">
               <Image
                   src="/dashboard.png"
                   alt="Dashboard"
                   width={1200}
                   height={800}
-                  className="rounded-lg"
+                  className="rounded-2xl"
                 />
             </button>
           </div>
@@ -71,7 +58,7 @@ const ComingSoon = () => {
 
 
 
-      <h1 className="font-inter font-medium md:text-5xl text-4xl leading-4xl tracking-tighter text-white bg-clip-text text-center transition-opacity duration-1000 delay-1500 mx-6 mt-20">
+      <h1 className="font-inter font-medium md:text-6xl text-4xl leading-4xl tracking-tighter text-white bg-clip-text text-center transition-opacity duration-1000 delay-1500 mx-6 mt-20">
         Aisq is coming soon to the public.
       </h1>
       <div className={`font-inter font-normal md:text-2xl text-xl leading-2xl text-gray-400 mt-5 text-center mx-6 transition-opacity duration-1000 delay-1000 ${showContent ? 'opacity-100' : 'opacity-0'}`}>
